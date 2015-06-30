dd769232b9edd5703dadbb822707054e8a891358
Gh0stBlade <misternightmare@live.com>
2015-06-30 15:12:37 +0100

    Add new lua script

---
+++ scripts/config/assets_constants.lua
@@ -0,0 +1,13 @@
+-- OPENTOMB ASSETS CONSTANTS LIST
+-- by Gh0stBlade, Jun 2015
+
+--------------------------------------------------------------------------------
+-- Responsible for storing information regarding known platforms/file versions
+--------------------------------------------------------------------------------
+
+PLATFORM_PC = 0x00; -- PC
+PLATFORM_SAT = 0x01; -- Sega Saturn
+PLATFORM_PSX = 0x02; -- PlayStation
+PLATFORM_NGE = 0x03; -- Nokia N-Gage
+PLATFORM_PPC = 0x04; -- Pocket PC
+PLATFORM_DC = 0x05; -- Dreamcast
